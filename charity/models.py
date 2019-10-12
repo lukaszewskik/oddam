462516c41319f4d612dc554185d43d64485c133a
Karol Łukaszewski <kk.lukaszewski@gmail.com>
2019-10-12 11:08:37 +0200

    Initial commit

---
+++ charity/models.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
+from django.db import models
+
+# Create your models here.
+TYPES = ((1, 'Fundacja'),
+         (2, 'Organizacja pozarządowa'),
+         (3, 'Zbiórka lokalna'))
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_regex = RegexValidator(regex=r'^\+?\d{9,15}$',
+                                 message="Numer telefonu musi być w formacie: '+999999999'. Max. 15 cyfr")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    city = models.CharField(max_length=64)
+    zip_code_regex = RegexValidator(regex=r'^\d{2}-\d{3}$',
+                                    message="Kod pocztowy musi być w formacie: '00-000'.")
+    zip_code = models.CharField(validators=[zip_code_regex], max_length=6, blank=True)
+    pick_up_date = models.DateField(auto_now_add=False)
+    pick_up_time = models.DateField(auto_now_add=False)
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
