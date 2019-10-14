c61b5da4c891ea48eeea730e7a280533c443458d
Karol Łukaszewski <kk.lukaszewski@gmail.com>
2019-10-14 15:27:53 +0200

    Implemented user registration

--- charity/views.py
+++ charity/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
@@ -29,8 +31,11 @@ class LandingPage(View):
         return render(request, "index.html", ctx)
 
 
+
 class AddDonation(View):
     def get(self, request):
+        if request.user.is_authenticated:
+            print('hej')
         return render(request, "form.html")
 
 
@@ -38,7 +43,36 @@ class Login(View):
     def get(self, request):
         return render(request, "login.html")
 
+    def post(self, request):
+        email = request.POST['email']
+        password = request.POST['password']
+        user = authenticate(username=email, password=password)
+        user = authenticate(username='zefireq', password='zefireq4')
+        print(email, password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect("landing_page")
+        else:
+            return redirect("register")
 
 class Register(View):
     def get(self, request):
         return render(request, "register.html")
+
+    def post(self, request):
+        name = request.POST.get("name")
+        surname = request.POST.get("surname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        print(name, surname)
+        if password == password2:
+            # TODO: check if user exists
+            User.objects.create_user(first_name=name,
+                                     last_name=surname,
+                                     email=email,
+                                     password=password,
+                                     username=email)
+            # User.objects.create_user()
+        return redirect("login")
